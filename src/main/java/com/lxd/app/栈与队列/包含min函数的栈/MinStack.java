a63c97c925cb08e4528318bbc68438930d0bb721
liuxinda <liuxinda@autohome.com.cn>
2023-07-06 18:22:56 +0800

    feat(add): 栈和队列
    包含min函数的栈

--- src/main/java/com/lxd/app/栈与队列/包含min函数的栈/MinStack.java
+++ src/main/java/com/lxd/app/栈与队列/包含min函数的栈/MinStack.java
@@ -1,44 +1,39 @@
 package com.lxd.app.栈与队列.包含min函数的栈;
 
-import java.util.LinkedList;
-import java.util.List;
+import java.util.Stack;
 
 class MinStack {
 
-    int min = Integer.MIN_VALUE;
-
-    List<Integer> stack;
+    Stack<Integer> stack;
+    Stack<Integer> minStack;
 
     /**
      * initialize your data structure here.
      */
     public MinStack() {
-        stack = new LinkedList<>();
+        stack = new Stack<>();
+        minStack = new Stack<>();
+        minStack.push(Integer.MAX_VALUE);
     }
 
     public void push(int x) {
-        if (x < min) {
-            min = x;
-        }
-        stack.add(x);
+        stack.push(x);
+        minStack.push(Math.min(minStack.peek(), x));
     }
 
     public void pop() {
-        if (stack.isEmpty()) {
-            return;
-        }
-        Integer remove = stack.remove(stack.size() - 1);
-//        if (remove ) {
-//        }
+        stack.pop();
+        minStack.pop();
     }
 
     public int top() {
-        return stack.get(stack.size() - 1);
+        return stack.peek();
     }
 
     public int min() {
-        return min;
+        return minStack.peek();
     }
+
 }
 
 /**
