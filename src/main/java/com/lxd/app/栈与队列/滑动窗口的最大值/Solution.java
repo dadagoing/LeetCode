9eb5c95dfe98aec52cb9f719d63d050c422e57cd
lxd <aizijiplease@gmail.com>
2023-07-08 18:48:08 +0800

    feat(add): 滑动窗口的最大值
    维护单调队列，保证一次遍历结束，暴力解可以实现，但是时间复杂度过高

--- src/main/java/com/lxd/app/栈与队列/滑动窗口的最大值/Solution.java
+++ src/main/java/com/lxd/app/栈与队列/滑动窗口的最大值/Solution.java
@@ -1,10 +1,6 @@
 package com.lxd.app.栈与队列.滑动窗口的最大值;
 
-import java.util.ArrayDeque;
-import java.util.Arrays;
-import java.util.Queue;
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.function.IntConsumer;
+import java.util.*;
 
 /**
  * 给定一个数组nums和滑动窗口的大小 k，请找出所有滑动窗口里的最大值。
@@ -33,7 +29,6 @@ class Solution {
     }
 
     public int[] maxSlidingWindow(int[] nums, int k) {
-        Queue<Integer> queue = new ArrayDeque<>(k);
         if (nums == null || nums.length == 0) {
             return nums;
         }
@@ -41,9 +36,29 @@ class Solution {
             return new int[]{Arrays.stream(nums).max().getAsInt()};
         }
         int[] result = new int[nums.length - k + 1];
-        for (int i = 0; i < nums.length - k + 1; i++) {
-            result[i] = Arrays.stream(Arrays.copyOfRange(nums, i, i + k)).max().getAsInt();
+        Deque<Integer> deque = new LinkedList<>();
+        int head = 0;
+        for (int i = 0; i < nums.length; i++) {
+            pushItem(deque, nums[i]);
+            if (i >= k - 1) {
+                result[i - k + 1] = deque.peekFirst();
+                if (nums[head] == deque.peekFirst()) {
+                    deque.removeFirst();
+                }
+                head++;
+            }
         }
         return result;
+    }
+
+    private void pushItem(Deque<Integer> deque, int nums) {
+        if (deque.isEmpty()) {
+            deque.offer(nums);
+        } else if (deque.peekLast() < nums) {
+            deque.removeLast();
+            pushItem(deque, nums);
+        } else {
+            deque.offer(nums);
+        }
     }
 }
