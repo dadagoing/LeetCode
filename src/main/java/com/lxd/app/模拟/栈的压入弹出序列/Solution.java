ba5b7f323dc31d3b33cd10f2ab122b981f4ce4ce
lxd <aizijiplease@gmail.com>
2023-07-09 23:26:39 +0800

    feat(add): 三数之和 && 栈的压入弹出序列
    临时思路，待完善

---
+++ src/main/java/com/lxd/app/模拟/栈的压入弹出序列/Solution.java
@@ -0,0 +1,43 @@
+package com.lxd.app.模拟.栈的压入弹出序列;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Stack;
+import java.util.stream.Collectors;
+
+class Solution {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.validateStackSequences(new int[]{0, 1}, new int[]{0, 1}));
+    }
+
+    public boolean validateStackSequences(int[] pushed, int[] popped) {
+        if (popped.length != pushed.length) {
+            return false;
+        }
+        HashSet<Integer> hashSet = Arrays.stream(pushed).boxed().collect(Collectors.toCollection(HashSet::new));
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0, poppedLength = popped.length; i < poppedLength; i++) {
+            int pop = popped[i];
+            if (!hashSet.contains(pop)) {
+                return false;
+            }
+            if (i == 0 && stack.isEmpty()) {
+                for (int push : pushed) {
+                    if (push == pop) {
+                        break;
+                    }
+                    stack.push(push);
+                }
+            }
+            if (stack.peek() == pop) {
+                stack.pop();
+            } else {
+                if (popped[i] != pushed[i]) {
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+}
