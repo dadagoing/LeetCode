8505c77667aabba49782d4a521ea4fcd56dee877
liuxinda <liuxinda@autohome.com.cn>
2023-07-10 15:43:20 +0800

    feat(add): 栈的压入弹出序列
    模拟入栈出栈操作

--- src/main/java/com/lxd/app/模拟/栈的压入弹出序列/Solution.java
+++ src/main/java/com/lxd/app/模拟/栈的压入弹出序列/Solution.java
@@ -8,34 +8,23 @@ import java.util.stream.Collectors;
 class Solution {
     public static void main(String[] args) {
         Solution solution = new Solution();
-        System.out.println(solution.validateStackSequences(new int[]{0, 1}, new int[]{0, 1}));
+        System.out.println(solution.validateStackSequences(new int[]{1, 2, 3, 4, 5}, new int[]{4, 5, 3, 2, 1}));
     }
 
     public boolean validateStackSequences(int[] pushed, int[] popped) {
         if (popped.length != pushed.length) {
             return false;
         }
-        HashSet<Integer> hashSet = Arrays.stream(pushed).boxed().collect(Collectors.toCollection(HashSet::new));
         Stack<Integer> stack = new Stack<>();
-        for (int i = 0, poppedLength = popped.length; i < poppedLength; i++) {
-            int pop = popped[i];
-            if (!hashSet.contains(pop)) {
-                return false;
-            }
-            if (i == 0 && stack.isEmpty()) {
-                for (int push : pushed) {
-                    if (push == pop) {
-                        break;
-                    }
-                    stack.push(push);
+        int index = 0;
+        for (int i = 0; i < pushed.length; i++) {
+            stack.push(pushed[i]);
+            while (true){
+                if (stack.empty() || stack.peek() != popped[index]) {
+                    break;
                 }
-            }
-            if (stack.peek() == pop) {
                 stack.pop();
-            } else {
-                if (popped[i] != pushed[i]) {
-                    return false;
-                }
+                index++;
             }
         }
         return stack.isEmpty();
