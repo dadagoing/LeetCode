7c987393aa67dc1081e84423db3ab77bcea17106
liuxinda <liuxinda@autohome.com.cn>
2023-08-07 16:42:18 +0800

    feat(add): 字符串的排列

---
+++ src/main/java/com/lxd/app/Test.java
@@ -0,0 +1,8 @@
+package com.lxd.app;
+
+public class Test {
+    public static void main(String[] args) {
+        String price = "6.18";
+        System.out.println(Double.parseDouble(price));
+    }
+}
